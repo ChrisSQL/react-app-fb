3fc7fdd2b964624f98567a550e1688b5873568ff
Chris Maher <chrismaher.wit@gmail.com>
2019-07-06 22:05:14 +0100

    fb

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import FacebookLogin from 'react-facebook-login';
+
+import GoogleLogin from 'react-google-login';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+
+        const responseFacebook = (response) => {
+            console.log(response);
+        }
+
+        const responseGoogle = (response) => {
+            console.log(response);
+        }
+
+        return (
+            <div className="App">
+                <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+                <FacebookLogin
+                    appId="2009920755997639" //APP ID NOT CREATED YET
+                    fields="name,email,picture"
+                    callback={responseFacebook}
+                />
+                <br />
+                <br />
+
+
+                <GoogleLogin
+                    clientId="" //CLIENTID NOT CREATED YET
+                    buttonText="LOGIN WITH GOOGLE"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                />
+
+            </div>
+        );
+    }
 }
 
 export default App;
